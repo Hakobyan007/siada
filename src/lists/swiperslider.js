a0821c96856da9657916402be0de4edd259a3711
Hakobyan007 <095510007var.h@mail.ru>
2023-11-10 15:32:36 +0400

    supermarket

---
+++ src/lists/swiperslider.js
@@ -0,0 +1,38 @@
+export const swiperslider = [
+    {
+        id : Math.random(),
+        pic : '../img/slide1.jpg',
+        name : 'Aloe Natural Drink',
+        text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat fugit deserunt asperiores consequuntur quisquam quas labore temporibus neque consectetur, aliquam harum at pariatur quidem quo mollitia ex atque adipisci'
+    },
+    {
+        id : Math.random(),
+        pic : '../img/slide2.jpg',
+        name : 'Pantene Summer Edition',
+        text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat fugit deserunt asperiores consequuntur quisquam quas labore temporibus neque consectetur, aliquam harum at pariatur quidem quo mollitia ex atque adipisci'
+    },
+    {
+        id : Math.random(),
+        pic : '../img/slide4.webp',
+        name : 'Doritos Nacho',
+        text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat fugit deserunt asperiores consequuntur quisquam quas labore temporibus neque consectetur, aliquam harum at pariatur quidem quo mollitia ex atque adipisci'
+    },
+    {
+        id : Math.random(),
+        pic : '../img/slide6.jpg',
+        name : 'Schwarzkopf Shampoo',
+        text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat fugit deserunt asperiores consequuntur quisquam quas labore temporibus neque consectetur, aliquam harum at pariatur quidem quo mollitia ex atque adipisci'
+    },
+    {
+        id : Math.random(),
+        pic : '../img/slide7.jpg',
+        name : 'Dr. Severin',
+        text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat fugit deserunt asperiores consequuntur quisquam quas labore temporibus neque consectetur, aliquam harum at pariatur quidem quo mollitia ex atque adipisci'
+    },
+    {
+        id : Math.random(),
+        pic : '../img/slide10.jpg',
+        name : 'Fanta',
+        text : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat fugit deserunt asperiores consequuntur quisquam quas labore temporibus neque consectetur, aliquam harum at pariatur quidem quo mollitia ex atque adipisci'
+    },
+]
---
+++ src/pages/home/category.js
@@ -0,0 +1,46 @@
+export const category = [
+    {
+        name: 'Drinks',
+        picture: 'https://wallpapers.com/images/hd/bar-alcoholic-drinks-6fj3udabe3l4smcp.jpg'
+    },
+    {
+        name: 'Dairy',
+        picture: 'https://techbullion.com/wp-content/uploads/2022/07/Fermented-Dairy-Ingredients-Market.jpg'
+    },
+    {
+        name: 'Seafood',
+        picture: 'https://img4.goodfon.ru/original/3000x2001/1/b5/moreprodukty-sol-zelen-krevetki-perets-tomat.jpg'
+    },
+    {
+        name: 'Fruits',
+        picture: 'https://img5.goodfon.com/original/2800x1766/9/91/vinograd-perets-ovoshchi-frukty-mandariny-pomidory.jpg'
+    },
+    {
+        name: 'Cakes',
+        picture: 'https://wallpapercrafter.com/sizes/1920x1080/139535-dark-sweets-food-cup-berries-dessert-cake-coffee-mint-leaves.jpg'
+    },
+    {
+        name: 'Sweets',
+        picture: 'https://w.forfun.com/fetch/85/857913564a38655d439aea66b9950b33.jpeg'
+    },
+    {
+        name: 'Juice',
+        picture: 'https://i.pinimg.com/originals/25/e7/52/25e752c3736e31b600f1d64ced69394d.jpg'
+    },
+    {
+        name: 'Bakery',
+        picture: 'https://kartinkin.net/uploads/posts/2021-07/1625221609_46-kartinkin-com-p-pekarnya-fon-krasivie-foni-55.jpg'
+    },
+    {
+        name: 'Meat',
+        picture: 'https://hidhabalsham.com/wp-content/uploads/2020/12/halal-meat.jpg'
+    },
+    {
+        name: 'Groceries',
+        picture: 'https://w.forfun.com/fetch/61/6135dbf32bd89f93806e51ca42098627.jpeg'
+    },
+    {
+        name: 'Spices',
+        picture: 'https://www.dpcedcenter.org/wp-content/uploads/2020/12/Herbs-and-spices-for-cooking-on-dark-background-941858854_2122x1416.jpeg'
+    },  
+];
